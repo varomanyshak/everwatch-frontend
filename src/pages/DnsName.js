3484759126404578f1dd7bda5b8e7201a5638f0b
varomanyshak <varomanyshak@gmail.com>
2024-06-23 11:50:08 -0300

    page/update data on the table and modal

--- src/pages/DnsName.js
+++ src/pages/DnsName.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button } from '@themesberg/react-bootstrap';
 import { Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux'
+import DetailModal from '../components/DetailModal';
+
 const DnsName = (props) => {
+    const Tabledata = JSON.parse(useSelector((state) => state.counter.value)).DNS
+    const [modalShow, setModalShow] = useState(false);
+    const [detailObj, setdetailObj] = useState({
+        confidence: "",
+        id: "",
+        module: "",
+        parsed: "",
+        scan: "",
+        scan_id: "",
+        scans: "",
+        timestamp: "",
+        _BaseEvent__host: "",
+        _BaseEvent__words: "",
+        _data: {
+            domains: [],
+            "tenant-id": "",
+            "tenant-names": []
+        },
+        _dummy: "",
+        _hash: "",
+        _id: "",
+        _internal: "",
+        _module_priority: "",
+        _port: "",
+        _priority: "",
+        _resolved_hosts: "",
+        _scope_distance: "",
+        _source: "",
+        _source_id: "",
+        _stats_recorded: "",
+        _tags: "",
+        _type: ""
+    });
+
+    const listDetail = (list) => {
+        setdetailObj(list)
+        setModalShow(true)
+    }
+    
     return (
         <>
             <Container style={{
@@ -11,18 +53,24 @@ const DnsName = (props) => {
                 'marginTop': '10px'
             }}>
 
-                {props.Tabledata.map((item, index) => (
+                {Tabledata.map((item, index) => (
                     <Card style={{ width: '18rem' }} key={index}>
                         <Card.Body>
                             <Card.Title>{item.timestamp}</Card.Title>
                             <Card.Text>
                                 {item._data}
                             </Card.Text>
-                            <Button variant="primary">More detail</Button>
+                            <Button variant="primary" onClick={() => listDetail(item)}>More detail</Button>
                         </Card.Body>
                     </Card>
                 ))}
             </Container>
+            <DetailModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                data={detailObj}
+                type="dns"
+            />
         </>
     )
 };
