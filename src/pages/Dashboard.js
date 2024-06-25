a34b6b98354a12c0d7dae7541e33640564961202
varomanyshak <varomanyshak@gmail.com>
2024-06-26 00:09:30 +0300

    update/dashboard-basic-structrue-firstStep

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Chart from "react-apexcharts";
+import { Col, Row, } from '@themesberg/react-bootstrap';
 import filterData from "../sheet";
 
 let timeData = [];
-for(const key in filterData) {    
+for (const key in filterData) {
   let newFilter = {}
   if (Array.isArray(filterData[key])) {
-    newFilter[key] = filterData[key].map(obj => {            
+    newFilter[key] = filterData[key].map(obj => {
       return {
         x: key,
-        y : [new Date(obj.timestamp).getTime(), new Date(obj.timestamp).getTime() + eval(500)]
+        y: [new Date(obj.timestamp).getTime(), new Date(obj.timestamp).getTime() + eval(500)]
       };
     });
-    
+
     timeData = timeData.concat(newFilter[key])
   }
 }
@@ -20,44 +21,248 @@ for(const key in filterData) {
 
 
 const ApexChart = () => {
-    const linedata = {
-      series: [
-        {
-          data:timeData
-        }
-      ],
-      options: {
-        chart: {
-          height: 500,
-          type: 'rangeBar'
-        },
-        plotOptions: {
-          bar: {
-            horizontal: true
-          }
-        },
-        xaxis: {
-          type: 'datetime'
+  const linedata = {
+    series: [
+      {
+        data: timeData
+      }
+    ],
+    options: {
+      chart: {
+        height: 500,
+        type: 'rangeBar'
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true
         }
       },
-    }
-      
-    return (
-      <div>
-        <div id="chart">
-          <Chart options={linedata.options} series={linedata.series} type="rangeBar" height={350} />
-        </div>
-        <div id="html-dist"></div>
-      </div>
-    )
+      xaxis: {
+        type: 'datetime'
+      }
+    },
   }
-  
+
+  return (
+    <div>
+      <div id="chart">
+        <Chart options={linedata.options} series={linedata.series} type="rangeBar" height={350} />
+      </div>
+      <div id="html-dist"></div>
+    </div>
+  )
+}
+
+
+export function barChart() {
+  const barstate = {
+    series: [{
+      name: 'Inflation',
+      data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+    }],
+    options: {
+      chart: {
+        height: 350,
+        type: 'bar',
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 10,
+          dataLabels: {
+            position: 'top', // top, center, bottom
+          },
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function (val) {
+          return val + "%";
+        },
+        offsetY: -20,
+        style: {
+          fontSize: '12px',
+          colors: ["#304758"]
+        }
+      },
+
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        position: 'top',
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        crosshairs: {
+          fill: {
+            type: 'gradient',
+            gradient: {
+              colorFrom: '#D8E3F0',
+              colorTo: '#BED1E6',
+              stops: [0, 100],
+              opacityFrom: 0.4,
+              opacityTo: 0.5,
+            }
+          }
+        },
+        tooltip: {
+          enabled: true,
+        }
+      },
+      yaxis: {
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false,
+        },
+        labels: {
+          show: false,
+          formatter: function (val) {
+            return val + "%";
+          }
+        }
+
+      },
+      title: {
+        text: 'Monthly Inflation in Argentina, 2002',
+        floating: true,
+        offsetY: 330,
+        align: 'center',
+        style: {
+          color: '#444'
+        }
+      }
+    }
+  };
+  return (
+    <div id="chart">
+      <Chart options={barstate.options} series={barstate.series} type="bar" height={350} />
+    </div>
+  );
+}
+
 const Dashboard = (props) => {
-      return (
-        <>
-          <ApexChart />
-        </>
-      )
+  const barstate = {
+    series: [{
+      name: 'Inflation',
+      data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+    }],
+    options: {
+      chart: {
+        height: 350,
+        type: 'bar',
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 10,
+          dataLabels: {
+            position: 'top', // top, center, bottom
+          },
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function (val) {
+          return val + "%";
+        },
+        offsetY: -20,
+        style: {
+          fontSize: '12px',
+          colors: ["#304758"]
+        }
+      },
+
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        position: 'top',
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        crosshairs: {
+          fill: {
+            type: 'gradient',
+            gradient: {
+              colorFrom: '#D8E3F0',
+              colorTo: '#BED1E6',
+              stops: [0, 100],
+              opacityFrom: 0.4,
+              opacityTo: 0.5,
+            }
+          }
+        },
+        tooltip: {
+          enabled: true,
+        }
+      },
+      yaxis: {
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false,
+        },
+        labels: {
+          show: false,
+          formatter: function (val) {
+            return val + "%";
+          }
+        }
+
+      },
+      title: {
+        text: 'Monthly Inflation in Argentina, 2002',
+        floating: true,
+        offsetY: 330,
+        align: 'center',
+        style: {
+          color: '#444'
+        }
+      }
+    }
+  };
+
+  const Cirstate = {
+    series: [44, 55, 41, 17, 15],
+    options: {
+      chart: {
+        type: 'donut',
+      },
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+    },
+
+
+  };
+  return (
+    <>
+      <Row>
+        <ApexChart />
+      </Row>
+      <Row>
+        <Col>
+          <Chart options={barstate.options} series={barstate.series} type="bar" height={350} />
+        </Col>
+        <Col>
+          <Chart options={Cirstate.options} series={Cirstate.series} type="donut" height={350} />
+        </Col>
+      </Row>
+
+    </>
+  )
 };
 
 export default Dashboard;
